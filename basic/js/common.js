f0fdbde21d7ae8ff57f978cf5f6b3c8f7e84a9a7
masaccioi <masaccioi@163.com>
2017-03-01 11:48:23 +0800

    basic

--- basic/js/common.js
+++ basic/js/common.js
@@ -1,5 +1,28 @@
 window.onload = function () {
-	document.getElementsByClassName('navbar-toggle').onclick = function () {
-	document.getElementsByClassName('collapse').toggle('in')
+  const collapse = document.getElementsByClassName('collapse')
+  const toggle = document.getElementsByClassName('navbar-toggle')
+  toggle[0].onclick = function () {
+    for (let i = 0; i < collapse.length; i++) {
+      toggleClass(collapse[i], 'in')
+    }
+  }
+}
+function hasClass (obj, cls) {
+  return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'))
+}
+function addClass (obj, cls) {
+  if (!this.hasClass(obj, cls)) obj.className += ' ' + cls
+}
+function removeClass (obj, cls) {
+  if (hasClass(obj, cls)) {
+    const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)')
+    obj.className = obj.className.replace(reg, ' ')
+  }
+}
+function toggleClass (obj, cls) {
+  if (hasClass(obj, cls)) {
+    removeClass(obj, cls)
+  } else {
+    addClass(obj, cls)
   }
 }
